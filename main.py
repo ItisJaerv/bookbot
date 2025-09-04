05b38f6b20747f0a02cde5427584a13bff176f0f
Hans Jörg <hainrichjoachim@gmail.com>
2025-09-04 20:24:58 +0200

    Added new function of character count to stats, imported nwe function into main

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from stats import countWords
+from stats import countChars
 
 def main():
     words = countWords("books/frankenstein.txt")
     print(words, "words found in the document")
+    character = countChars("books/frankenstein.txt")
+    print(character)
 
 
 main()   
--- stats.py
+++ stats.py
@@ -1,11 +1,21 @@
-def getBookText(filePath):
+def getBookText(filePath):                  #Function reads txt and returns string
     with open(filePath) as file:
         fileContent = file.read()
     return fileContent
     
-def countWords(filePath):
+def countWords(filePath):                   #Function reads txt, creates a List of all words, and returns List length
     with open(filePath) as file:
         fileContent = file.read()
         wordList = fileContent.split()
         Counter = len(wordList)
     return Counter
+
+def countChars(filePath):                   #Function reads txt, creates a string, lowers all capitals, returns a dict, with count of each character
+    with open(filePath) as file:
+        fileContent = file.read()
+        fileContent = fileContent.lower()
+        charCounter = {"a": fileContent.count("a"), "b": fileContent.count("b"), "c": fileContent.count("c"), "d": fileContent.count("d"), "e": fileContent.count("e"), "f": fileContent.count("f"), 
+        "g": fileContent.count("g"), "h": fileContent.count("h"), "i": fileContent.count("i"), "j": fileContent.count("j"), "k": fileContent.count("k"), "l": fileContent.count("l"), "m": fileContent.count("m"), 
+        "n": fileContent.count("n"), "o": fileContent.count("o"), "p": fileContent.count("p"), "q": fileContent.count("q"), "r": fileContent.count("r"), "s": fileContent.count("s"), "t": fileContent.count("t"), 
+        "u": fileContent.count("u"), "v": fileContent.count("v"), "w": fileContent.count("w"), "x": fileContent.count("x"), "y": fileContent.count("y"), "z": fileContent.count("z")}
+    return charCounter
